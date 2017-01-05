d490f6a6d19edd0d94c7ca58bdb3fbb823464022
Jesús Escamilla <jesusescamilla@hotmail.com>
2017-01-05 17:33:22 -0600

    new methods on categories

--- Controllers/CategoriesController.cs
+++ Controllers/CategoriesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using dotnetpractice.Models.ViewModels.Categories;
+using dotnetpractice.Models.ViewModels.Products;
 
 namespace dotnetpractice.Controllers
 {
@@ -17,6 +18,14 @@ namespace dotnetpractice.Controllers
         [Route("Categories/{Slug}")]
         public IActionResult Details(string Slug)
         {
+            CategoriesVM category = new CategoriesVM
+            {
+                Slug = "tvs",
+                Name = "TV's",
+                Description = "A big TV"
+            };
+            ViewBag.Category = category;
+            ViewBag.Products = GetProducts();
             return View();
         }
 
@@ -32,6 +41,16 @@ namespace dotnetpractice.Controllers
                 new CategoriesVM { Slug = "pcs", Name = "PC's", Description = "The best PC's" },
                 new CategoriesVM { Slug = "appliances", Name = "Home Appliances", Description = "For your Home" },
                 new CategoriesVM { Slug = "games", Name = "Games", Description = "Best Entertaiment" }
+            };
+        }
+
+        private static List<ProductsVM> GetProducts()
+        {
+            return new List<ProductsVM>
+            {
+                new ProductsVM { Id = 1, Name = "Macboc Pro", Price = 2513.45 },
+                new ProductsVM { Id = 1, Name = "Sharp TV", Price = 599.99 },
+                new ProductsVM { Id = 1, Name = "Acer Aspire", Price = 435.19 },
             };
         }
     }
