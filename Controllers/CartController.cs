c878eb75136235798d5d9e288fad4c08400920e1
Jesús Escamilla <jesusescamilla@hotmail.com>
2017-01-05 23:21:44 -0600

    cart html

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using dotnetpractice.Models.ViewModels.Products;
 
 namespace dotnetpractice.Controllers
 {
@@ -10,12 +11,22 @@ namespace dotnetpractice.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View(GetProducts());
         }
 
         public IActionResult Error()
         {
             return View();
+        }
+
+        private static List<ProductsVM> GetProducts()
+        {
+            return new List<ProductsVM>
+            {
+                new ProductsVM { Id = 1, Name = "Macboc Pro", Price = 2513.45 },
+                new ProductsVM { Id = 2, Name = "Sharp TV", Price = 599.99 },
+                new ProductsVM { Id = 3, Name = "Acer Aspire", Price = 435.19 },
+            };
         }
     }
 }
